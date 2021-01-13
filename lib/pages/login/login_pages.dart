87c21e8cafbb9556fb144f4c3214624ee1360c88
lfgalviz <lgalviz@uninorte.edu.co>
2021-01-13 15:00:05 -0500

    Signed-off-by: lfgalviz <lgalviz@uninorte.edu.co>

--- lib/pages/login/login_pages.dart
+++ lib/pages/login/login_pages.dart
@@ -14,6 +14,7 @@ class _LoginPageState extends State<LoginPage> {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark);
   }
 
+//comentario
   @override
   Widget build(BuildContext context) {
     return Scaffold(
